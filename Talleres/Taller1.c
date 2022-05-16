683fbe9f3640438424b8c98500ffcf31555a2333
Francisco Solis <fran@theprogramsrc.xyz>
2022-05-16 08:25:04 -0400

    Fixed Taller1 & Updated OnlineGDB Links

--- Talleres/Taller1.c
+++ Talleres/Taller1.c
@@ -1,4 +1,4 @@
-/*
+/* 
     Integrante 1: Mauricio Reynoso
     Integrante 2: Francisco Solis
     Profesor(a): Freddy Marambio
@@ -26,25 +26,22 @@ int main() {
     }
 
     int identificador = 0, // Identificador de un asistente
-    numeroDias = 0, // Cantidad de días que se estará (entre 1 y 3)
-    edad = 0, // Edad del asistente
-    contadorDias1 = 0, // Cantidad de personas que asisten 1 dia
-    contadorDiasMenores1 = 0, // Cantidad de personas que asisten 1 dia para menores
-    contadorDias2 = 0, // cantidad de personas que asisten 2 dias
-    contadorDiasMenores2 = 0, // cantidad de personas que asisten 2 dias para menores
-    contadorDias3 = 0,  // Cantidad de personas que asisten 3 dias
-    contadorDiasMenores3 = 0,  // Cantidad de personas que asisten 3 dias para menores
-    sumaEdad = 0 // Suma de las edades de los asistentes que van al evento por 2 dias
+        numeroDias = 0, // Cantidad de días que se estará (entre 1 y 3)
+        edad = 0, // Edad del asistente
+        contadorDias1 = 0, // Cantidad de personas que asisten 1 dia
+        contadorDias2 = 0, // cantidad de personas que asisten 2 dias
+        contadorDias3 = 0,  // Cantidad de personas que asisten 3 dias
+        sumaEdad = 0, // Suma de las edades de los asistentes que van al evento por 2 dias
+        idMenor = 0, // Identificador de la persona con menor edad
+        edadMenor = 0, // La menor edad entre todas las personas
+        asistentesA2Dias = 0 // Cantidad de personas que asisten 2 días
     ;
 
     float promedioAsistencia = 0.0, // Promedio de dias de asistencia
             promedioEdad = 0.0; // Promedio de edad de quienes van por 2 dias
 
-    char respuesta; // Respuesta de si va con menor de edad
-
     int tempAsistentes = asistentes; // Variable temporal
     for (int i = 1; i <= tempAsistentes; i++) { // Loop por cada asistente
-
         // 1. Solicitar identificador
         printf("Ingresa un identificador para el asistente #%d (debe ser mayor a 0): ", i);
         scanf("%d", &identificador);
@@ -55,7 +52,7 @@ int main() {
         }
 
         // 2. Solicitar numero de días que se estará
-        printf("\nCuantos días estará en el evento? (Ingresa un número entre 1 y 3): ");
+        printf("Cuantos días estará en el evento? (Ingresa un número entre 1 y 3): ");
         scanf("%d", &numeroDias);
         while (numeroDias < 1 || numeroDias > 3) { // Si el usuario ingresa un numero distinto a 1, 2 ó 3
             flushInput();
@@ -64,79 +61,38 @@ int main() {
         }
 
         if (numeroDias == 1) {
-            contadorDias1++;
+            contadorDias1++; // Incrementa en 1 el contadorDias1
         } else if (numeroDias == 2) {
-            contadorDias2 += 2;
+            contadorDias2 += 2; // incrementa en 2 el contadorDias2
         } else {
-            contadorDias3 += 3;
+            contadorDias3 += 3; // incrementa en 3 el contadorDias3
         }
 
         // 3. Solicitar la edad del asistente
-        printf("\nCuál es la edad del asistente? (Ingresar un número entre 18 y 135): ");
+        printf("Cuál es la edad del asistente? (Ingresar un número entre 1 y 135): ");
         scanf("%d", &edad);
-        while (edad < 18 || edad > 135) { // Si el usuario ingresa un numero menor a 18 o mayor a 135
+        while (edad < 1 || edad > 135) { // Si el usuario ingresa un numero menor a 1 o mayor a 135
             flushInput();
-            printf("Por favor ingresa un número entre 18 y 135: ");
+            printf("Por favor ingresa un número entre 1 y 135: ");
             scanf("%d", &edad);
         }
 
         if (numeroDias == 2) {
             sumaEdad += edad;
+            asistentesA2Dias++;
         }
 
-        // 4. Preguntar si va acompañado de un menor de edad
-        printf("Va acompañado de un menor de edad? (s/n): ");
-        scanf(" %s", &respuesta);
-        if(respuesta == 's') {
-            asistentes++;
-            // Solicitar identificador
-            printf("Ingresa un identificador para el menor de edad (debe ser mayor a 0): ");
-            scanf("%d", &identificador);
-            while (identificador <= 0) { // En caso de que se ingrese un numero menor a 0
-                flushInput();
-                printf("Por favor ingresa un número mayor a 0: ");
-                scanf("%d", &identificador);
-            }
-
-            // Solicitar cantidad de días a estar
-            printf("\nCuantos días estará el menor de edad en el evento? (Ingresa un número entre 1 y 3): ");
-            scanf("%d", &numeroDias);
-            while (numeroDias < 1 || numeroDias > 3) { // Si el usuario ingresa un numero distinto a 1, 2 ó 3
-                flushInput();
-                printf("Por favor ingresa un número entre 1 y 3: ");
-                scanf("%d", &numeroDias);
-            }
-    
-            if (numeroDias == 1) {
-                contadorDiasMenores1++;
-            } else if (numeroDias == 2) {
-                contadorDiasMenores2+=2;
-            } else {
-                contadorDiasMenores3+=3;
-            }
-
-            // preguntar la edad para estadisticas nadamas
-            if (numeroDias == 2) {
-                printf("\nCuál es la edad del asistente? (Ingresar un número entre 1 y 18): ");
-                scanf("%d", &edad);
-                while (edad < 1 || edad > 18) { // Si el usuario ingresa un numero menor a 1 o mayor a 18
-                    flushInput();
-                    printf("Por favor ingresa un número entre 1 y 18: ");
-                    scanf("%d", &edad);
-                }
-
-                sumaEdad += edad;
-            }
+        if(edad < edadMenor || edadMenor == 0) {
+            idMenor = identificador;
+            edadMenor = edad;
         }
-
-        respuesta = 'n';
 
         printf("\n\n");
     }
 
-    promedioAsistencia = (contadorDias1 + contadorDias2 + contadorDias3 + contadorDiasMenores1 + contadorDiasMenores2 + contadorDiasMenores3) / ((float) asistentes);
-    promedioEdad = sumaEdad / ((float) asistentes);
-    int recaudado = (contadorDias1 + contadorDias2 + contadorDias3 + contadorDiasMenores1 + contadorDiasMenores2 + contadorDiasMenores3) * 25000;
+    promedioAsistencia = (contadorDias1 + contadorDias2 + contadorDias3) / ((float) asistentes);
+    promedioEdad = sumaEdad / ((float) asistentesA2Dias);
+    int recaudado = (contadorDias1 + contadorDias2 + contadorDias3) * 25000;
 
     printf("\n===============\n");
     printf("Promedio de dias de asistencia: %.2f", promedioAsistencia);
@@ -144,7 +100,9 @@ int main() {
         printf("\nPromedio de edad de quienes asistieron 2 días: %.2f", promedioEdad);
     }
     printf("\nRecaudación: $%d Pesos", recaudado);
+    printf("\nLa persona de menor edad tiene ID: %d y su edad es %d", idMenor, edadMenor);
     printf("\n===============\n");
 
     return 0;
 }
+
