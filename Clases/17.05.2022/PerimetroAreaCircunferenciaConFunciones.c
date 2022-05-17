567dc24d044d35bb4f5b52e62c476a624937bcb0
Francisco Solis <fran@theprogramsrc.xyz>
2022-05-17 10:31:10 -0400

    Updated Links & Dates

--- Clases/17.05.2022/PerimetroAreaCircunferenciaConFunciones.c
+++ Clases/17.05.2022/PerimetroAreaCircunferenciaConFunciones.c
@@ -1,5 +1,5 @@
 /*
- * Created by Francisco Solis Maturana on 19-04-22.
+ * Created by Francisco Solis Maturana on 17-05-22.
  *
  * Escriba un programa que reciba el valor del radio de una circunferencia y retorne el respectivo perimetro y area
  */
