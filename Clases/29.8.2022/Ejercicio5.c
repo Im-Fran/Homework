e3d4ab53e6078d2b115f1a05a79640cc54831041
Francisco Solis <fran@theprogramsrc.xyz>
2022-09-10 20:14:48 -0300

    Added new stuff

---
+++ Clases/29.8.2022/Ejercicio5.c
@@ -0,0 +1,18 @@
+/*
+ * Created by Francisco Solis Maturana on 8/29/2022.
+ *
+ * Implementar una funcion que intercambie los digitios extremos de 2 numeros naturales n, m, ambos mayores
+ * que 100, es decir que intercambie el primer digito de n con el ultimo de m y el ultimo de n con el primero
+ * de m, conservando los restantes en su posicion; por ejemplo, si inicialmente n = 123 y m=43459, finalmente
+ * n=924 y m=33451
+ */
+
+#include <stdio.h>
+
+void intercambiar(int *a){
+}
+
+int main(){
+    int n = 123, m = 43459;
+    
+}
